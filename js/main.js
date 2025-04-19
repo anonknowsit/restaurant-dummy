1b24d4ae9ac173668ec537a8135715e01ceae061
AnonknowsIT <anonknowsit@outlook.de>
2025-04-19 15:23:21 +0200

    adjust website

--- js/main.js
+++ js/main.js
@@ -75,3 +75,44 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+let slideIndex = 1;
+let slideTimer = null;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlides(slideIndex);
+  slideTimer = setTimeout(nextSlide, 6000);
+}
+
+function currentSlide(n) {
+  clearTimeout(slideTimer); 
+  slideIndex = n;
+  showSlides(slideIndex);
+  slideTimer = setTimeout(nextSlide, 6000); 
+}
+
+// Initialize the slideshow
+window.onload = function() {
+  showSlides(slideIndex);
+  slideTimer = setTimeout(nextSlide, 6000);
+};
